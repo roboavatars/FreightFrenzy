08e7515e3e8cb25081cfac698eb890c2329d3183
= <=>
2021-11-15 23:12:50 -0800

    reorganized april tag code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV/AprilTag/AprilTagDetector.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.OpenCV.AprilTag;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.OpenCV.BaseDetector;
+
+@Config
+public class AprilTagDetector extends BaseDetector {
+
+    private AprilTagPipeline pipeline;
+
+    public AprilTagDetector(LinearOpMode op, double tagsize, double fx, double fy, double cx, double cy) {
+        super(op);
+
+        pipeline = new AprilTagPipeline(tagsize);
+        setPipeline(pipeline);
+    }
+
+    public double[] getTagLocations () {
+        return pipeline.getLocation();
+    }
+}
